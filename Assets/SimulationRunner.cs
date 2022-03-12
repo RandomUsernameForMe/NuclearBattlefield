1be6dde5443318d186f347e0f308124f0ac7c9a6
guth.jarkovsky.tomas@gmail.com <guth.jarkovsky.tomas@gmail.com>
2022-03-12 02:39:35 +0100

    started creating simulations

---
+++ Assets/SimulationRunner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationRunner : MonoBehaviour
+{
+    void Simulate(int number)
+    {
+        List<Result> results;
+        var party = GameObject.Find("AllyParty");
+        for (int i = 0; i < 100; i++)
+        {
+            generate
+        }
+    }
+}
